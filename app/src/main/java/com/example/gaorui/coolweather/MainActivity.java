2592b166d8b2b684a45afff002f0588571b0cbfe
Gao Rui <gaorui@Gaos-MacBook-Pro.local>
2017-04-24 18:14:20 +0800

    First commit

---
+++ app/src/main/java/com/example/gaorui/coolweather/MainActivity.java
@@ -0,0 +1,13 @@
+package com.example.gaorui.coolweather;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
