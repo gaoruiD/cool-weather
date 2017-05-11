e76eba9b744993ea655641a52b97cf6090d593ca
Gao Rui <gaorui@Gaos-MacBook-Pro.local>
2017-05-11 19:04:57 +0800

    Change the city

--- app/src/main/java/com/example/gaorui/coolweather/ChooseAreaFragment.java
+++ app/src/main/java/com/example/gaorui/coolweather/ChooseAreaFragment.java
@@ -107,11 +107,17 @@ public class ChooseAreaFragment extends Fragment {
                     queryCounties();
                 } else if (currentLevel == LEVEL_COUNTY) {
                     String weatherId = countyList.get(position).getWeatherId();
-                    int countyId = countyList.get(position).getId();
-                    Intent intent = new Intent(getActivity(), WeatherActivity.class);
-                    intent.putExtra("weather_id", weatherId);
-                    startActivity(intent);
+                    if (getActivity() instanceof MainActivity) {
+                        int countyId = countyList.get(position).getId();
+                        Intent intent = new Intent(getActivity(), WeatherActivity.class);
+                        intent.putExtra("weather_id", weatherId);
+                        startActivity(intent);
 //                    getActivity().finish();
+                    } else if (getActivity() instanceof WeatherActivity) {
+                        WeatherActivity activity = (WeatherActivity) getActivity();
+                        activity.drawerLayout.closeDrawers();
+                        activity.requestWeather(weatherId);
+                    }
                 }
             }
         });
--- app/src/main/java/com/example/gaorui/coolweather/WeatherActivity.java
+++ app/src/main/java/com/example/gaorui/coolweather/WeatherActivity.java
@@ -2,7 +2,11 @@ package com.example.gaorui.coolweather;
 
 import android.content.Intent;
 import android.content.SharedPreferences;
+import android.graphics.Color;
+import android.os.Build;
 import android.preference.PreferenceManager;
+import android.support.v4.view.GravityCompat;
+import android.support.v4.widget.DrawerLayout;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.text.Layout;
@@ -10,11 +14,13 @@ import android.view.KeyEvent;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.widget.Button;
+import android.widget.ImageView;
 import android.widget.LinearLayout;
 import android.widget.ScrollView;
 import android.widget.TextView;
 import android.widget.Toast;
 
+import com.bumptech.glide.Glide;
 import com.example.gaorui.coolweather.gson.Forecast;
 import com.example.gaorui.coolweather.gson.Weather;
 import com.example.gaorui.coolweather.util.HttpUtil;
@@ -52,12 +58,24 @@ public class WeatherActivity extends AppCompatActivity {
 
     private TextView sportText;
 
-    private Button backToCountyButton;
+    private ImageView bingPicImg;
+
+    public DrawerLayout drawerLayout;
+
+    private Button navButton;
 
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        //将背景图和状态栏融合到一起
+        if (Build.VERSION.SDK_INT >= 21) {
+            View decorView = getWindow().getDecorView();
+            //表示activity的布局会显示在状态栏上面
+            decorView.setSystemUiVisibility(View.SYSTEM_UI_FLAG_FULLSCREEN | View.SYSTEM_UI_FLAG_LAYOUT_STABLE);
+            getWindow().setStatusBarColor(Color.TRANSPARENT); //将状态栏设置成透明色
+        }
+
         setContentView(R.layout.activity_weather);
         //初始化各种控件
         weatherLayout = (ScrollView) findViewById(R.id.weather_layout);
@@ -71,11 +89,48 @@ public class WeatherActivity extends AppCompatActivity {
         comfortText = (TextView) findViewById(R.id.comfort_text);
         carWashText = (TextView) findViewById(R.id.car_wash_text);
         sportText = (TextView) findViewById(R.id.sport_text);
-        backToCountyButton = (Button) findViewById(R.id.back_to_county_button);
+        bingPicImg = (ImageView) findViewById(R.id.bing_pic_img);
+        drawerLayout = (DrawerLayout) findViewById(R.id.drawer_layout);
+        navButton = (Button) findViewById(R.id.nav_button);
+        loadBingPic();
+
+        navButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                drawerLayout.openDrawer(GravityCompat.START);
+            }
+        });
 
         String weatherId = getIntent().getStringExtra("weather_id");
         weatherLayout.setVisibility(View.INVISIBLE);
         requestWeather(weatherId);
+    }
+
+    /**
+     * 加载必应每日一图
+     */
+    private void loadBingPic() {
+        String requestBingPic = "http://guolin.tech/api/bing_pic";
+        HttpUtil.sendOkHttpRequest(requestBingPic, new Callback() {
+            @Override
+            public void onFailure(Call call, IOException e) {
+                e.printStackTrace();
+            }
+
+            @Override
+            public void onResponse(Call call, Response response) throws IOException {
+                final String bingPic = response.body().string();
+                SharedPreferences.Editor editor = PreferenceManager.getDefaultSharedPreferences(WeatherActivity.this).edit();
+                editor.putString("bing_pic", bingPic);
+                editor.apply();
+                runOnUiThread(new Runnable() {
+                    @Override
+                    public void run() {
+                        Glide.with(WeatherActivity.this).load(bingPic).into(bingPicImg);
+                    }
+                });
+            }
+        });
     }
 
     @Override
@@ -99,7 +154,7 @@ public class WeatherActivity extends AppCompatActivity {
      * 根据weather ID请求城市天气信息
      * @param weatherId
      */
-    private void requestWeather(String weatherId) {
+    public void requestWeather(String weatherId) {
         String weatherUrl = "http://guolin.tech/api/weather?cityid=" + weatherId + "&key=2553af98e3974e2a90cf7f22c710a3e4";
         HttpUtil.sendOkHttpRequest(weatherUrl, new Callback() {
             @Override
@@ -130,6 +185,7 @@ public class WeatherActivity extends AppCompatActivity {
                 });
             }
         });
+        loadBingPic();
     }
 
     /**
